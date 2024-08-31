24df02883d8052011ee1b32f86a3600ecad6f3d2
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-08-31 15:07:52 +0600

    feat: remove env

--- server/di/di.go
+++ server/di/di.go
@@ -2,9 +2,6 @@ package di
 
 import (
 	_ "github.com/lib/pq" // <------------ here
-	"github.com/toufiq-austcse/go-api-boilerplate/internal/api/todo/controller"
-	"github.com/toufiq-austcse/go-api-boilerplate/internal/api/todo/repository"
-	"github.com/toufiq-austcse/go-api-boilerplate/internal/api/todo/service"
 	"github.com/toufiq-austcse/go-api-boilerplate/pkg/db/providers/mongodb"
 	"go.uber.org/dig"
 	"os/exec"
@@ -15,9 +12,6 @@ func NewDiContainer() (*dig.Container, error) {
 	providers := []interface {
 	}{
 		mongodb.New,
-		repository.NewRepository,
-		service.NewTodoService,
-		controller.NewTodoController,
 	}
 	for _, provider := range providers {
 		if err := c.Provide(provider); err != nil {
