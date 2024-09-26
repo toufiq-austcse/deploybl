6d09c2007816fe3b6d3f1d40d7358e165e89deb9
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-09-27 02:19:55 +0600

    fixing path issue

--- server/pkg/utils/util.go
+++ server/pkg/utils/util.go
@@ -1,7 +1,19 @@
 package utils
 
-import deployItConfig "github.com/toufiq-austcse/deployit/config"
+import (
+	deployItConfig "github.com/toufiq-austcse/deployit/config"
+	"github.com/toufiq-austcse/deployit/internal/api/deployments/worker/payloads"
+)
 
-func GetLocalRepoPath(deploymentID string) string {
-	return deployItConfig.AppConfig.REPOSITORIES_PATH + "/" + deploymentID
+func GetDockerBuildContextPath(payload payloads.BuildRepoWorkerPayload) string {
+	if payload.RootDir == nil {
+		return deployItConfig.AppConfig.REPOSITORIES_PATH + "/" + payload.DeploymentId
+	}
+	return deployItConfig.AppConfig.REPOSITORIES_PATH + "/" + payload.DeploymentId + "/" + *payload.RootDir
+
+}
+
+func GetLocalRepoPath(DeploymentId string) string {
+	return deployItConfig.AppConfig.REPOSITORIES_PATH + "/" + DeploymentId
+
 }
