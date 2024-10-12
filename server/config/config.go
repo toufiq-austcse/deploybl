1222fff1e3412133dfbd26f243d521801d038f38
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-10-13 01:11:11 +0600

    refactor: refactor config file

--- server/config/config.go
+++ server/config/config.go
@@ -2,65 +2,63 @@ package config
 
 import (
 	"fmt"
-
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
-)
-
-const (
-	defaultPort                = "3000"
-	defaultAppName             = "Boilerplate"
-	defaultDBDebugEnabledValue = "false"
 )
 
 type (
 	Config struct {
-		PORT                 string
-		APP_NAME             string
-		APP_URL              string
-		GITHUB_API_TOKEN     string
-		GITHUB_API_BASE_URL  string
-		DB_DRIVER_NAME       string
+		PORT                 string `env:"PORT"                 envDefault:"3000"`
+		APP_NAME             string `env:"APP_NAME"             envDefault:"Boilerplate"`
+		APP_URL              string `env:"APP_URL"`
+		GITHUB_API_TOKEN     string `env:"GITHUB_API_TOKEN"`
+		GITHUB_API_BASE_URL  string `env:"GITHUB_API_BASE_URL"`
+		DB_DRIVER_NAME       string `env:"DB_DRIVER_NAME"`
 		DB_CONFIG            DB_CONFIG
 		MONGO_DB_CONFIG      MONGO_DB_CONFIG
 		RABBIT_MQ_CONFIG     RABBIT_MQ_CONFIG
-		REPOSITORIES_PATH    string
-		BASE_DOMAIN          string
-		TRAEFIK_NETWORK_NAME string
+		REPOSITORIES_PATH    string `env:"REPOSITORIES_PATH"`
+		BASE_DOMAIN          string `env:"BASE_DOMAIN"`
+		TRAEFIK_NETWORK_NAME string `env:"TRAEFIK_NETWORK_NAME"`
 	}
 	DB_CONFIG struct {
-		DB_NAME       string
-		HOST          string
-		PORT          string
-		USER          string
-		PASSWORD      string
-		DEBUG_ENABLED string
+		DB_NAME       string `env:"DB_NAME"`
+		HOST          string `env:"DB_HOST"`
+		PORT          string `env:"DB_PORT"`
+		USER          string `env:"DB_USER"`
+		PASSWORD      string `env:"DB_PASSWORD"`
+		DEBUG_ENABLED string `env:"DEBUG_ENABLED"`
 	}
 	MONGO_DB_CONFIG struct {
-		URL     string
-		DB_NAME string
+		URL     string `env:"MONGO_DB_URL"`
+		DB_NAME string `env:"MONGO_DB_NAME"`
 	}
 	RABBIT_MQ_CONFIG struct {
-		URL                          string
-		EXCHANGE                     string
-		REPOSITORY_PULL_ROUTING_KEY  string
-		REPOSITORY_PULL_QUEUE        string
-		REPOSITORY_BUILD_ROUTING_KEY string
-		REPOSITORY_BUILD_QUEUE       string
-		REPOSITORY_RUN_ROUTING_KEY   string
-		REPOSITORY_RUN_QUEUE         string
-		REPOSITORY_STOP_ROUTING_KEY  string
-		REPOSITORY_STOP_QUEUE        string
+		URL                          string `env:"RABBIT_MQ_CONNECTION_URL"`
+		EXCHANGE                     string `env:"RABBIT_MQ_EXCHANGE"`
+		REPOSITORY_PULL_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_PULL_ROUTING_KEY"`
+		REPOSITORY_PULL_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_PULL_QUEUE"`
+		REPOSITORY_BUILD_ROUTING_KEY string `env:"RABBIT_MQ_REPOSITORY_BUILD_ROUTING_KEY"`
+		REPOSITORY_BUILD_QUEUE       string `env:"RABBIT_MQ_REPOSITORY_BUILD_QUEUE"`
+		REPOSITORY_RUN_ROUTING_KEY   string `env:"RABBIT_MQ_REPOSITORY_RUN_ROUTING_KEY"`
+		REPOSITORY_RUN_QUEUE         string `env:"RABBIT_MQ_REPOSITORY_RUN_QUEUE"`
+		REPOSITORY_STOP_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_STOP_ROUTING_KEY"`
+		REPOSITORY_STOP_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_STOP_QUEUE"`
 	}
 )
 
 var AppConfig Config
 
-func Init(envFilePath string) {
-	populateDefault()
-	parseConfigFile(envFilePath, "main")
-	setFromEnv(&AppConfig)
+func Init(envFilePath string) error {
+	err := parseConfigFile(envFilePath, "main")
+	if err != nil {
+		return err
+	}
+	fmt.Println(AppConfig)
 
 	fmt.Println("Configuration loaded")
+	return nil
 }
 
 func setFromEnv(config *Config) {
@@ -107,24 +105,10 @@ func setFromEnv(config *Config) {
 	config.TRAEFIK_NETWORK_NAME = viper.GetString("TRAEFIK_NETWORK_NAME")
 }
 
-func parseConfigFile(envFilePath, configName string) {
-	viper.SetConfigType("env")
-	viper.SetConfigName(configName)
-	viper.SetConfigFile(envFilePath)
-
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("error in reading config ", err)
-	}
-	viper.AutomaticEnv()
-
-	err := viper.MergeInConfig()
+func parseConfigFile(envFilePath, configName string) error {
+	err := godotenv.Load(envFilePath)
 	if err != nil {
-		fmt.Println("err in merge config", err)
+		return err
 	}
-}
-
-func populateDefault() {
-	viper.SetDefault("PORT", defaultPort)
-	viper.SetDefault("APP_NAME", defaultAppName)
-	viper.SetDefault("DEBUG_ENABLED", defaultDBDebugEnabledValue)
+	return env.Parse(&AppConfig)
 }
