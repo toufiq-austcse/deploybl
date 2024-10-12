4ffa9011da75161accb408ad0f6e14e12b449bb7
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-10-13 01:11:22 +0600

    refactor: refactor config file

--- server/config/config.go
+++ server/config/config.go
@@ -2,9 +2,9 @@ package config
 
 import (
 	"fmt"
+
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
-	"github.com/spf13/viper"
 )
 
 type (
@@ -51,61 +51,15 @@ type (
 var AppConfig Config
 
 func Init(envFilePath string) error {
-	err := parseConfigFile(envFilePath, "main")
+	err := parseConfigFile(envFilePath)
 	if err != nil {
 		return err
 	}
-	fmt.Println(AppConfig)
-
 	fmt.Println("Configuration loaded")
 	return nil
 }
 
-func setFromEnv(config *Config) {
-	config.PORT = viper.GetString("PORT")
-	config.APP_NAME = viper.GetString("APP_NAME")
-	config.APP_URL = viper.GetString("APP_URL")
-	config.GITHUB_API_TOKEN = viper.GetString("GITHUB_API_TOKEN")
-	config.GITHUB_API_BASE_URL = viper.GetString("GITHUB_API_BASE_URL")
-	config.DB_CONFIG.DB_NAME = viper.GetString("DB_NAME")
-	config.DB_CONFIG.HOST = viper.GetString("DB_HOST")
-	config.DB_CONFIG.PORT = viper.GetString("DB_PORT")
-	config.DB_CONFIG.PASSWORD = viper.GetString("DB_PASSWORD")
-	config.DB_DRIVER_NAME = viper.GetString("DB_DRIVER_NAME")
-	config.DB_CONFIG.USER = viper.GetString("DB_USER")
-	config.DB_CONFIG.DEBUG_ENABLED = viper.GetString("DB_DEBUG_ENABLED")
-	config.MONGO_DB_CONFIG.URL = viper.GetString("MONGO_DB_URL")
-	config.MONGO_DB_CONFIG.DB_NAME = viper.GetString("MONGO_DB_NAME")
-	config.RABBIT_MQ_CONFIG.EXCHANGE = viper.GetString("RABBIT_MQ_EXCHANGE")
-	config.RABBIT_MQ_CONFIG.URL = viper.GetString("RABBIT_MQ_CONNECTION_URL")
-	config.RABBIT_MQ_CONFIG.REPOSITORY_PULL_ROUTING_KEY = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_PULL_ROUTING_KEY",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_PULL_QUEUE = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_PULL_QUEUE",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_BUILD_ROUTING_KEY = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_BUILD_ROUTING_KEY",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_BUILD_QUEUE = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_BUILD_QUEUE",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_RUN_QUEUE = viper.GetString("RABBIT_MQ_REPOSITORY_RUN_QUEUE")
-	config.RABBIT_MQ_CONFIG.REPOSITORY_RUN_ROUTING_KEY = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_RUN_ROUTING_KEY",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_STOP_QUEUE = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_STOP_QUEUE",
-	)
-	config.RABBIT_MQ_CONFIG.REPOSITORY_STOP_ROUTING_KEY = viper.GetString(
-		"RABBIT_MQ_REPOSITORY_STOP_ROUTING_KEY",
-	)
-	config.REPOSITORIES_PATH = viper.GetString("REPOSITORIES_PATH")
-	config.BASE_DOMAIN = viper.GetString("BASE_DOMAIN")
-	config.TRAEFIK_NETWORK_NAME = viper.GetString("TRAEFIK_NETWORK_NAME")
-}
-
-func parseConfigFile(envFilePath, configName string) error {
+func parseConfigFile(envFilePath string) error {
 	err := godotenv.Load(envFilePath)
 	if err != nil {
 		return err
