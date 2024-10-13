a4acfc22b2e73bafca04b2f46acf643678e8d411
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-10-13 19:26:12 +0600

    refactor: config load

--- server/config/config.go
+++ server/config/config.go
@@ -9,18 +9,16 @@ import (
 
 type (
 	Config struct {
-		PORT                 string `env:"PORT"                 envDefault:"3000"`
-		APP_NAME             string `env:"APP_NAME"             envDefault:"Boilerplate"`
+		PORT                 string `env:"PORT"                          envDefault:"3000"`
+		APP_NAME             string `env:"APP_NAME"                      envDefault:"Boilerplate"`
 		APP_URL              string `env:"APP_URL"`
-		GITHUB_API_TOKEN     string `env:"GITHUB_API_TOKEN"`
-		GITHUB_API_BASE_URL  string `env:"GITHUB_API_BASE_URL"`
-		DB_DRIVER_NAME       string `env:"DB_DRIVER_NAME"`
-		DB_CONFIG            DB_CONFIG
+		GITHUB_API_TOKEN     string `env:"GITHUB_API_TOKEN,required"`
+		GITHUB_API_BASE_URL  string `env:"GITHUB_API_BASE_URL,required"`
 		MONGO_DB_CONFIG      MONGO_DB_CONFIG
 		RABBIT_MQ_CONFIG     RABBIT_MQ_CONFIG
-		REPOSITORIES_PATH    string `env:"REPOSITORIES_PATH"`
-		BASE_DOMAIN          string `env:"BASE_DOMAIN"`
-		TRAEFIK_NETWORK_NAME string `env:"TRAEFIK_NETWORK_NAME"`
+		REPOSITORIES_PATH    string `env:"REPOSITORIES_PATH,required"`
+		BASE_DOMAIN          string `env:"BASE_DOMAIN,required"`
+		TRAEFIK_NETWORK_NAME string `env:"TRAEFIK_NETWORK_NAME,required"`
 	}
 	DB_CONFIG struct {
 		DB_NAME       string `env:"DB_NAME"`
@@ -31,20 +29,20 @@ type (
 		DEBUG_ENABLED string `env:"DEBUG_ENABLED"`
 	}
 	MONGO_DB_CONFIG struct {
-		URL     string `env:"MONGO_DB_URL"`
-		DB_NAME string `env:"MONGO_DB_NAME"`
+		URL     string `env:"MONGO_DB_URL,required"`
+		DB_NAME string `env:"MONGO_DB_NAME,required"`
 	}
 	RABBIT_MQ_CONFIG struct {
-		URL                          string `env:"RABBIT_MQ_CONNECTION_URL"`
-		EXCHANGE                     string `env:"RABBIT_MQ_EXCHANGE"`
-		REPOSITORY_PULL_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_PULL_ROUTING_KEY"`
-		REPOSITORY_PULL_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_PULL_QUEUE"`
-		REPOSITORY_BUILD_ROUTING_KEY string `env:"RABBIT_MQ_REPOSITORY_BUILD_ROUTING_KEY"`
-		REPOSITORY_BUILD_QUEUE       string `env:"RABBIT_MQ_REPOSITORY_BUILD_QUEUE"`
-		REPOSITORY_RUN_ROUTING_KEY   string `env:"RABBIT_MQ_REPOSITORY_RUN_ROUTING_KEY"`
-		REPOSITORY_RUN_QUEUE         string `env:"RABBIT_MQ_REPOSITORY_RUN_QUEUE"`
-		REPOSITORY_STOP_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_STOP_ROUTING_KEY"`
-		REPOSITORY_STOP_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_STOP_QUEUE"`
+		URL                          string `env:"RABBIT_MQ_CONNECTION_URL,required"`
+		EXCHANGE                     string `env:"RABBIT_MQ_EXCHANGE,required"`
+		REPOSITORY_PULL_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_PULL_ROUTING_KEY,required"`
+		REPOSITORY_PULL_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_PULL_QUEUE,required"`
+		REPOSITORY_BUILD_ROUTING_KEY string `env:"RABBIT_MQ_REPOSITORY_BUILD_ROUTING_KEY,required"`
+		REPOSITORY_BUILD_QUEUE       string `env:"RABBIT_MQ_REPOSITORY_BUILD_QUEUE,required"`
+		REPOSITORY_RUN_ROUTING_KEY   string `env:"RABBIT_MQ_REPOSITORY_RUN_ROUTING_KEY,required"`
+		REPOSITORY_RUN_QUEUE         string `env:"RABBIT_MQ_REPOSITORY_RUN_QUEUE,required"`
+		REPOSITORY_STOP_ROUTING_KEY  string `env:"RABBIT_MQ_REPOSITORY_STOP_ROUTING_KEY,required"`
+		REPOSITORY_STOP_QUEUE        string `env:"RABBIT_MQ_REPOSITORY_STOP_QUEUE,required"`
 	}
 )
 
