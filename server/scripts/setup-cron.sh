fb6ba3513a1a9dcac1effdf5d7af4d2b8fec8ba5
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-11-30 02:46:55 +0600

    feat: remove existing crontab if already exists

--- server/scripts/setup-cron.sh
+++ server/scripts/setup-cron.sh
@@ -2,6 +2,7 @@
 
 # Cron Setup Script for Curl Commands
 
+(crontab -l 2>/dev/null | grep -v "check-stopped-cron" | grep -v "check-deploying-cron") | crontab -
 # Setup cron jobs
 (crontab -l 2>/dev/null; echo "* * * * * curl --request POST --url 'http://localhost:3000/api/v1/deployments/check-stopped-cron'") | crontab -
 (crontab -l 2>/dev/null; echo "* * * * * curl --request POST --url 'http://localhost:3000/api/v1/deployments/check-deploying-cron'") | crontab -
